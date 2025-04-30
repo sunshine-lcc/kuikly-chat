be3987b6a8c6c733c003d6d01c573745246f2ea9
sunshine <luchangcheng2333@qq.com>
2025-04-30 20:19:19 +0800

    upgrade version and add icon and name

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 23
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
