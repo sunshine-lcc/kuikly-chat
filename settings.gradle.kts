6ccb1e0a1ba53cae99b364c6fc716a238843d809
sunshine <luchangcheng2333@qq.com>
2025-04-30 19:57:02 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,36 @@
+pluginManagement {
+    repositories {
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+        maven { setUrl("https://jitpack.io") }
+        maven { setUrl("https://maven.aliyun.com/nexus/content/groups/public/") }
+        maven { setUrl("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
+
+//        google()
+//        gradlePluginPortal()
+//        mavenCentral()
+//        mavenLocal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+        maven { setUrl("https://jitpack.io") }
+
+//        google()
+//        mavenCentral()
+//        mavenLocal()
+    }
+}
+
+rootProject.name = "MyApplication"
+include(":androidApp")
+include(":shared")
